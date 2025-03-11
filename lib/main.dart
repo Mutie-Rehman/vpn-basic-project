784200f2531385da942c97005415674864160461
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-11 19:20:34 +0500

    setup Ui mode and used getx also added primary themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:get/route_manager.dart';
+import 'package:vpn_basic_project/screens/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // Initialize the binding before using SystemChrome
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((v) {
+    runApp(const MyApp());
+  });
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'OpenVpn Demo',
-      home: HomeScreen(),
+      //theme
+      theme: ThemeData(
+        appBarTheme:
+            AppBarTheme(centerTitle: true, elevation: 3, color: Colors.blue),
+        primaryColor: Colors.blue,
+        primaryIconTheme: IconThemeData(color: Colors.blue),
+        primarySwatch: Colors.blue,
+        primaryTextTheme: TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+      ),
+      home: SplashScreen(),
     );
   }
 }
