ecccfd8158bc1e79a3d57072f4369dfbb75c9cd6
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-11 19:18:35 +0500

    home card widget added

---
+++ lib/widgets/home_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vpn_basic_project/global/global_variables.dart';
+
+class HomeCard extends StatelessWidget {
+  final String title, subtitle;
+  final Widget icon;
+
+  HomeCard(
+      {super.key,
+      required this.title,
+      required this.subtitle,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
+    return SizedBox(
+      width: mq.width * 0.45,
+      child: Column(
+        children: [
+          icon,
+          SizedBox(
+            height: mq.height * 0.005,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
+          ),
+          SizedBox(
+            height: mq.height * 0.005,
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(fontSize: 12, color: Colors.black54),
+          ),
+        ],
+      ),
+    );
+  }
+}
