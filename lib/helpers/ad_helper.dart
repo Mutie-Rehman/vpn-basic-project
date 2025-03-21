a1ac4ee9be841c6c5267dafaef1d5bf353bbdf03
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-21 15:13:09 +0500

    ad caching added

--- lib/helpers/ad_helper.dart
+++ lib/helpers/ad_helper.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:vpn_basic_project/controllers/native_ad_controller.dart';
+import 'package:vpn_basic_project/helpers/config.dart';
 import 'package:vpn_basic_project/helpers/my_dialogs.dart';
 
 class AdHelper {
@@ -13,17 +14,64 @@ class AdHelper {
     await MobileAds.instance.initialize();
   }
 
+  static InterstitialAd? _interstitialAd;
+  static bool _interstitialAddLoaded = false;
+
+  //pre load interstitial ad
+  static void preCacheInterstitialAd() {
+    log('Pre Cache Interstitial ad id: ${Config.interstitialAd}');
+    if (Config.hideAds) return;
+
+    InterstitialAd.load(
+      adUnitId: Config.interstitialAd,
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          ad.fullScreenContentCallback = FullScreenContentCallback(
+            onAdDismissedFullScreenContent: (ad) {
+              _resetInterstitialAd();
+              preCacheInterstitialAd();
+            },
+          );
+          _interstitialAd = ad;
+          _interstitialAddLoaded = true;
+        },
+        onAdFailedToLoad: (err) {
+          print('Failed to load an interstitial ad: ${err.message}');
+        },
+      ),
+    );
+  }
+
+  //reset interstitial ad
+  static void _resetInterstitialAd() {
+    _interstitialAd?.dispose();
+    _interstitialAd = null;
+    _interstitialAddLoaded = false;
+  }
+
   //interstitial ad
   static void showInterstitialAd({required VoidCallback onComplete}) {
+    log('Interstitial ad loaded: ${Config.interstitialAd}');
+    if (Config.hideAds) {
+      onComplete();
+      return;
+    }
+    if (_interstitialAddLoaded && _interstitialAd != null) {
+      _interstitialAd?.show();
+      onComplete();
+    }
     MyDialogs.showProgress();
     InterstitialAd.load(
-      adUnitId: "ca-app-pub-3940256099942544/1033173712",
+      adUnitId: Config.interstitialAd,
       request: AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (ad) {
           ad.fullScreenContentCallback = FullScreenContentCallback(
             onAdDismissedFullScreenContent: (ad) {
               onComplete();
+              _resetInterstitialAd();
+              preCacheInterstitialAd();
             },
           );
           Get.back();
