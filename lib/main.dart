f8b0eec19ec7ed86f954e717297f0f5a4298354c
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-21 15:14:25 +0500

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:vpn_basic_project/controllers/theme_controller.dart';
+import 'package:vpn_basic_project/firebase_options.dart';
 import 'package:vpn_basic_project/helpers/ad_helper.dart';
+import 'package:vpn_basic_project/helpers/config.dart';
 import 'package:vpn_basic_project/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //initializing remote config
+  await Config.initConfig();
 
   // ✅ Initialize ThemeController before running the app
   Get.put(ThemeController());
